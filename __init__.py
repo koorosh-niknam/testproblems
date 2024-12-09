053a455fac09db5c05d230e9638bad7e395253eb
koorosh-niknam <122859011+koorosh-niknam@users.noreply.github.com>
2024-12-09 06:08:19 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <cs50.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdlib.h>
+include <stdio.h>
+include <cs50.h>
+include <string.h>
+include <ctype.h>
+include <stdlib.h>
 
 int main(int argc, string argv[])
 {
